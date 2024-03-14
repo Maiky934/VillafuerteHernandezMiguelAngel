dee272d37cbf5a833ac8ef422738fa40708129cd
Maiky934 <angel2002vh@gmail.com>
2024-03-13 23:42:51 -0600

    Tercer Commir

---
+++ 06JSIntermedio/funciones.js
@@ -0,0 +1,11 @@
+/*
+Las variables que se ocupan dentro de js son 3 tipos 
+
+var->de acuerdo a EJS6 esta siendo situada 
+*/
+
+const x = "x";
+if(true){
+    let x = "y";
+    console.log(x);
+}   
